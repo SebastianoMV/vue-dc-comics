b2b9cd18f4366bc90faa5ee7d650e8a0a85559b3
SebastianoMV <sebastianovisco@gmail.com>
2022-05-13 17:45:42 +0200

    impostazione footer

--- src/App.vue
+++ src/App.vue
@@ -29,5 +29,6 @@ export default {
 
 <style lang="scss">
 @import './assets/style/general';
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap');
 
 </style>
--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -3,9 +3,22 @@
       <!-- MAIN FOOTER -->
       <div class="main-footer">
         <div class="container">
-          <div>
+          <div class="ul-container-footer">
+            <div>
+                <h2>DC COMICS</h2>
+                <h2>SHOP</h2>
+            </div>
+            <div>
+              <h2>DC</h2>
+            
+            </div>
+            <div>
+              <h2>SITES</h2>
+
+            </div>
 
           </div>
+
           <div class="logo-bg">
 
           </div>
@@ -15,7 +28,19 @@
 
       <!-- FOOT FOOTER -->
       <div class="foot-footer">
-        <div class="container"></div>
+        <div class="container">
+          <div class="btn-footer">
+            <a href="#"> SING-UP NOW!</a>
+          </div>
+          <div class="social">
+            <h2>FOLLOW US</h2>
+            <img src="../assets/img/footer-facebook.png" alt="">
+            <img src="../assets/img/footer-periscope.png" alt="">
+            <img src="../assets/img/footer-pinterest.png" alt="">
+            <img src="../assets/img/footer-twitter.png" alt="">
+            <img src="../assets/img/footer-youtube.png" alt="">
+          </div>
+        </div>
 
       </div>
     
@@ -40,8 +65,42 @@ export default {
     display: flex;
     justify-content: space-between;
     height: 100%;
+    .ul-container-footer{
+      width: 40%;
+      height: 100%;
+      display: flex;
+      justify-content: space-between;
+      color: white;
+    }
   }
   
+}
+
+.foot-footer{
+  height: 100px;
+  background-color: #303030;
+  .container{
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .btn-footer{
+      border: 2px solid $primary-color;
+      padding: 10px;
+      a{
+        text-decoration: none;
+        color: white;
+      }
+       
+    };
+    .social{
+      display: flex;
+      align-items: center;
+      img{
+        margin-left: 15px;
+      }
+    }
+  }
 }
 
 
