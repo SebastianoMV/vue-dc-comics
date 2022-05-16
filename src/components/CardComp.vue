bc386eb722e9b382afa1d62271cf715006aded2d
SebastianoMV <sebastianovisco@gmail.com>
2022-05-16 16:29:58 +0200

    Main con card dinamiche

---
+++ src/components/CardComp.vue
@@ -0,0 +1,39 @@
+<template>
+   <div class="card"> 
+     <div class="card-img">
+       <img :src="card.thumb" :alt="card.series">
+     </div>
+     <h5>{{card.series}}</h5>
+    </div>
+      
+   
+</template>
+
+<script>
+
+export default {
+  name: "CardComp",
+  props:{
+    card:Object,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+.card{
+  margin-top: 40px;
+  .card-img{
+    overflow: hidden;
+    width: 200px;
+    height: 200px;
+  }
+  h5{
+    margin-top: 10px;
+    text-transform: uppercase;
+    width: 200px;
+  }
+  
+}
+
+</style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,23 +1,67 @@
 <template>
   <main>
-    <div class="container">
-      CONTENT
+    <div class="jumbo">
+      <div class="container">
+        <div class="btn-jumbo">Current Series</div>
+      </div>
     </div>
+    <div class="container">
+      <CardComp v-for="(card,index) in dataDc" :key="index" :card="card"/>
+    </div>
+    <div class="btn-more">Load more</div>
+    
+ 
    
   </main>
 </template>
 
 <script>
+import CardComp from './CardComp.vue';
+import dataDc from '../assets/data/dataDc'
+
 export default {
-  name:"MainComp"
+    name: "MainComp",
+    components: { CardComp },
+    data(){
+      return{
+        dataDc
+      }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "../assets/style/utilities";
+@import "../assets/style/var";
   main{
     background-color: #1c1c1c;
-    height: 100px; //da rimuovere
+    padding: 20px;
+    .container{
+      display: flex;
+      flex-wrap: wrap;
+    }
     color: white;
+    .jumbo{
+      position: relative;
+      height: 400px;
+      background-image: url(../assets/img/jumbotron.jpg);
+      .btn-jumbo{
+        position: absolute;
+        bottom: -20px;
+        padding: 10px 20px;
+        background-color: $primary-color;
+        text-transform: uppercase;
+        font-weight: bold;
+      }
+    }
+    .btn-more{
+      width: 120px;
+      padding: 10px 20px;
+      background-color: $primary-color;
+      text-transform: uppercase;
+      font-weight: bold;
+      margin: auto;
+      margin-top: 20px;
+    }
   }
 </style>
